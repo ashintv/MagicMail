596dce709c1375c5e5784b25773a125d52c3b21b
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2024-11-17 17:26:50 +0530

    read_for_deploy_1

--- app.py
+++ app.py
@@ -69,4 +69,5 @@ def gmail():
     return render_template('gmail.html', user=user)
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0',port=5000,)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
